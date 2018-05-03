b393349058cc5607ffa602461c3942c72f29e1dc
Дмитрий Цирульников <dmitry.tsiryulnikov@setinbox.com>
2018-05-03 23:56:09 +0300

    merge files model

--- src/model/index.js
+++ src/model/index.js
@@ -3,8 +3,6 @@ const isArray = require('../helper/is-array');
 const toJSON = require('../helper/to-json');
 const each = require('../helper/each');
 
-const makeModel = require('./make-model');
-
 
 const MODEL = Symbol('model');
 const KEYS = Symbol('keys');
@@ -14,8 +12,78 @@ const SUBSCRIBERS = Symbol('subscribers');
 const TRIGGER = Symbol('trigger');
 
 
+const METHODS = [
+    "concat", "copyWithin", "entries", "fill", "map", "pop", "push", "reduce",
+    "reduceRight", "reverse", "shift", "slice", "sort", "splice", "unshift"
+];
+
+
+function makeModel(item) {
+    if (isArray(item)) {
+        return new ArrayModel(...item);
+    }
+
+    if (isObject(item) && !(item instanceof Model)) {
+        return new Model(item);
+    }
+
+    return item;
+}
+
+
+class ArrayModel extends Array {
+
+    constructor(...args) {
+        args.map(item => {
+            return makeModel(item);
+        });
+
+        super(...args);
+
+        this[SUBSCRIBERS] = [];
+
+        METHODS.forEach(key => {
+            this[key] = (...args) => {
+                super[key](...args);
+                this[TRIGGER]();
+            }
+        });
+    }
+
+    /**
+     * Запуск подписчеков, после изменения модели
+     */
+    [TRIGGER]() {
+        each(this[SUBSCRIBERS], item => {
+            item(this);
+        });
+    }
+
+    patchValue(model) {
+        throw 'ArrayModel does not support the method patchValue';
+    }
+
+    subscribe(handler) {
+        this[SUBSCRIBERS].push(handler);
+        return this;
+    }
+
+    toObject() {
+        throw 'ArrayModel does not support the method toObject';
+    }
+
+    toForm() {
+        throw 'ArrayModel does not support the method toForm';
+    }
+}
+
+
 class Model {
     constructor(model) {
+        if (isArray(model)) {
+            return new ArrayModel(...model);
+        }
+
         if (!isObject(model)) {
             throw 'The model must be an object';
         }
